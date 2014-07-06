c50bd52dba904fedacbe58bf91e2dfd4b8ba7e4f
STAR_ZERO <kenji.01.star@gmail.com>
2014-07-06 14:18:22 +0900

    Add select history in peco at zsh

--- .zsh.d/lib/functions.zsh
+++ .zsh.d/lib/functions.zsh
@@ -95,3 +95,19 @@ precmd() {
         echo -n "\ek$(hostname -s)\e\\"
     fi
 }
+
+
+peco-select-history() {
+    typeset tac
+    if which tac > /dev/null; then
+        tac=tac
+    else
+        tac='tail -r'
+    fi
+    BUFFER=$(fc -l -n 1 | eval $tac | peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle -R -c
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
+
